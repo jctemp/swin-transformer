fix(transformer): masking causing nan values

model/modules/window_attention/window_attention_base.py
@@ -7,7 +7,6 @@ import torch
 import torch.nn as nn
 from einops import einsum, rearrange, repeat
 
-
 class WindowMultiHeadAttention(ABC, nn.Module):
 
     def __init__(
@@ -111,21 +110,16 @@ class WindowMultiHeadAttention(ABC, nn.Module):
                 ):
                     batch_diff = score.shape[0] // mask.shape[0]
                     mask = repeat(mask, "b h nw1 nw2 -> (b d) h nw1 nw2", d=batch_diff)
-                expected_shape = score.shape
-            elif mask_dims + 1 == score_dims:
-                expected_shape = score.shape[1:]
             else:
-                expected_shape = score.shape[2:]
-
-            if mask.shape != expected_shape:
                 raise ValueError(
-                    f"Mask shape {mask.shape} doesn't match expected shape {expected_shape}"
+                    f"len(score.shape) ({len(score.shape)}) != len(mask.shape) ({len(mask.shape)})"
                 )
 
-            score = score.masked_fill(mask == 0, float("-inf"))
+            score = score + mask
 
         # Compute attention weights
         context = self.softmax(score)
+        assert not torch.any(torch.isnan(context)), context
         context = self.drop_attn(context)
 
         # Attend values

tests/test_swin_transformer_block.py
@@ -24,7 +24,6 @@ class TestSwinTransformerBlock:
             "qkv_bias": True,
             "drop": 0.1,
             "drop_attn": 0.1,
-            "drop_proj": 0.1,
             "drop_path": 0.1,
             "rpe": True,
             "shift": False,
@@ -49,6 +48,7 @@ class TestSwinTransformerBlock:
             x = torch.randn(batch_size, block_params["in_channels"], 56, 56, 56)
 
         output = block(x)
+        assert not torch.any(torch.isnan(output))
 
         # Check output shape
         assert output.shape == x.shape
@@ -66,7 +66,8 @@ class TestSwinTransformerBlock:
         else:
             x = torch.randn(batch_size, block_params["in_channels"], 56, 56, 56)
 
-        _ = block(x)
+        output = block(x)
+        assert not torch.any(torch.isnan(output))
 
         # Check that attention map is stored
         assert block.attn_map is not None
@@ -85,6 +86,8 @@ class TestSwinTransformerBlock:
         
         output1 = block1(x)
         output2 = block2(x)
+        assert not torch.any(torch.isnan(output1))
+        assert not torch.any(torch.isnan(output2))
 
         assert not torch.allclose(output1, output2)
 

tests/test_window_attention.py
@@ -43,6 +43,8 @@ class TestWindowMultiHeadAttention:
             x = torch.randn(batch_size, attention_params["in_channels"], 56, 56, 56)
 
         output, context = attn(x, x, x)
+        assert not torch.any(torch.isnan(output))
+        assert not torch.any(torch.isnan(context))
 
         # Check output shape
         assert output.shape == x.shape
@@ -71,6 +73,8 @@ class TestWindowMultiHeadAttention:
             mask[:, :171, :171] = 0  # mask out roughly half
 
         output, context = attn(x, x, x, mask)
+        assert not torch.any(torch.isnan(output))
+        assert not torch.any(torch.isnan(context))
 
         # Check that masked areas have zero attention
         masked_area = 24 if dims == 2 else 171
@@ -88,11 +92,15 @@ class TestWindowMultiHeadAttention:
         else:
             x = torch.randn(batch_size, attention_params["in_channels"], 56, 56, 56)
 
-        output_rpe, _ = attn(x, x, x)
+        output_rpe, context = attn(x, x, x)
+        assert not torch.any(torch.isnan(output_rpe))
+        assert not torch.any(torch.isnan(context))
 
         attention_params["rpe"] = not rpe
         attn_no_rpe = Attention(**attention_params)
-        output_no_rpe, _ = attn_no_rpe(x, x, x)
+        output_no_rpe, context = attn_no_rpe(x, x, x)
+        assert not torch.any(torch.isnan(output_no_rpe))
+        assert not torch.any(torch.isnan(context))
 
         # Outputs should be different when rpe is changed
         assert not torch.allclose(output_rpe, output_no_rpe)
@@ -108,7 +116,9 @@ class TestWindowMultiHeadAttention:
         else:
             x = torch.randn(batch_size, attention_params["in_channels"], 56, 56, 56)
 
-        _, context = attn(x, x, x)
+        output, context = attn(x, x, x)
+        assert not torch.any(torch.isnan(output))
+        assert not torch.any(torch.isnan(context))
 
         # Check that attention values sum to 1 for each query, allowing for small numerical errors
         assert torch.allclose(context.sum(dim=-1), torch.ones_like(context.sum(dim=-1)), atol=1e-6, rtol=1e-5)
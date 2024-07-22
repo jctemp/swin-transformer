ef1e4bb5ee5406ff2db4f4e69ce3bfb8be7db208
Jamie Temple <jamie-temple@live.de>
2024-07-22 22:54:06 +0200

    fix(transformer): add 3d version to init

--- model/__init__.py
+++ model/__init__.py
@@ -1,6 +1,9 @@
 from .swin_transformer_2d import SwinTransformer2D, SwinTransformerConfig2D
+from .swin_transformer_3d import SwinTransformer3D, SwinTransformerConfig3D
 
 __all__ = [
     "SwinTransformer2D",
     "SwinTransformerConfig2D",
+    "SwinTransformer3D",
+    "SwinTransformerConfig3D",
 ]
